6bf25e314d6e4c689d077517611e3afcb4a65a09
Carlos Henckes <henckes@uol.com.br>
2021-09-29 08:54:55 -0300

    Update SensorsController.java

--- src/main/java/br/com/fatec/iot/SensorsController.java
+++ src/main/java/br/com/fatec/iot/SensorsController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,9 +24,15 @@ public class SensorsController {
 
 	@ResponseStatus(code = HttpStatus.OK)
 	@GetMapping
-	public String save(final DataDTO data) {
-		log.info("SensorsController.save - start. data: [{}]", data);
-		return service.saveSensorData(data);
+	public String save(@RequestParam("field1") final String field1, 
+					   @RequestParam("field2") final String field2, 
+					   @RequestParam("field3") final String field3) {
+		log.info("SensorsController.save - start. field1: [{}], field2: [{}], field3: [{}]", field1, field2, field3);
+		return service.saveSensorData(DataDTO.builder()
+				.field1(field1)
+				.field2(field2)
+				.field3(field3)
+				.build());
 	}
 	
 	@ResponseStatus(code = HttpStatus.OK)
